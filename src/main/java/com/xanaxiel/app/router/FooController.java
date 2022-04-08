14644e790ea32de0edca8eab05e0a5c3a13cfd5f
Cy Pangilinan <x.anaxiel-github@yahoo.com>
2022-04-09 00:09:45 +0800

    Add produces text/event-stream

--- src/main/java/com/xanaxiel/app/router/FooController.java
+++ src/main/java/com/xanaxiel/app/router/FooController.java
@@ -4,24 +4,28 @@ import com.xanaxiel.app.model.Foo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 
+import static org.springframework.http.ResponseEntity.ok;
+
 @RequiredArgsConstructor
 @RestController
+@ResponseBody
 @Slf4j
 public class FooController {
 
-    @GetMapping("/echo")
+    @GetMapping(value = "/echo", produces = "text/event-stream")
     public Flux<?> echo() {
         final Foo foo = Foo.defaultFoo();
         return Flux.push(emitter -> {
             while (!emitter.isCancelled()) {
                 log.info("Emitting: {}", foo);
-                emitter.next(foo);
+                emitter.next(ok(foo));
             }
             emitter.onCancel(() -> {
                 log.info("Cancelled");
